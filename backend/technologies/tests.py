383758a5bd5d7eab75f46104453c2b764df74d90
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-06-08 21:01:34 +0200

    Updated unit tests

--- backend/technologies/tests.py
+++ backend/technologies/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+import tempfile
+from django.test import TestCase
+from .models import Technology
 
-# Create your tests here.
+
+class TechnologyModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.tag = Technology.objects.create(
+            name='Django',
+            icon=tempfile.NamedTemporaryFile(suffix='.svg').name
+        )
+
+    def test_tag_model(self):
+        data = self.tag
+        self.assertIsInstance(data, Technology)
