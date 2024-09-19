71a43304e5998d54b2c53e317a7e1b6ec03eacf6
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-09-20 00:15:26 +0200

    Code refactoring

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import AOS from 'aos';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
